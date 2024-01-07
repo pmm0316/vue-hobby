b29e601a1a07332e298b6bd1697532d489bf7082
pmm0316 <18267914053@163.com>
2024-01-07 22:55:59 +0800

    feat: 引入gui，设置物体的属性

--- src/views/App_01.vue
+++ src/views/App_01.vue
@@ -2,8 +2,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import gsap from 'gsap'
-// 导入图形用户界面库
-import * as dat from 'dat.gui'
 
 // 创建一个场景
 const scene = new THREE.Scene()
@@ -16,7 +14,7 @@ camera.position.set(0, 0, 10)
 scene.add(camera)
 // 添加物体
 // 创建几何体
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({
   color: 0xffff00
 })
@@ -25,41 +23,14 @@ const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 // 将物体添加到场景中
 scene.add(cube)
 
-// 创建GUI
-const gui = new dat.GUI()
-gui
-  .add(cube.position, 'x')
-  .min(0)
-  .max(5) // 最大值
-  .step(0.1) // 步长
-  .name('移动x坐标')
-  .onChange((value) => {
-    console.log('onChange', value)
-  })
-  .onFinishChange((value) => {
-    console.log('onFinishChange', value)
-  })
-//修改物体的颜色
-const params = {
-  color: '#ffff00',
-  fn: () => {
-    // 让立方体运行起来
-    gsap.to(cube.position, { x: 5, duration: 2, yoyo: true, repeat: -1 })
-  }
-}
-// 设置颜色
-gui.addColor(params, 'color').onChange((value) => {
-  console.log('onChange-color', value)
-  cube.material.color.set(value)
-})
+// 修改物体的位置
+// cube.position.set(5, 0, 0)
+// cube.position.x = 2
+// 缩放
+// cube.scale.set(3, 2, 1)
+// 旋转
+// cube.rotation.set(Math.PI / 4, 0, 0)
 
-// 设置按钮点击 触发某个事件
-gui.add(params, 'fn').name('立方体运动')
-// 增加文件夹
-const folder = gui.addFolder('设置立方体')
-folder.add(cube.material, 'wireframe')
-// 设置选项框
-folder.add(cube, 'visible').name('是否显示')
 console.log(cube)
 
 // 初始化渲染器
@@ -69,8 +40,9 @@ renderer.setSize(innerWidth, innerHeight)
 // 将webgl旋绕的canvaas内容添加到body中
 document.body.appendChild(renderer.domElement)
 
+
 // 创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls( camera, renderer.domElement );
 // 设置阻尼感
 controls.enableDamping = true
 
@@ -82,20 +54,36 @@ scene.add(axesHelper)
 // 设置时钟
 const clock = new THREE.Clock()
 
+// 设置动画
+const animate = gsap.to(cube.position, {
+  x: 5,
+  duration: 5,
+  // 设置重复的次数
+  repeat: 2,
+  // 往返运动
+  yoyo: true,
+  // 延迟2s运动
+  delay: 2,
+  onComplete: () => {
+    console.log('动画完成')
+  },
+  onStart: () => {
+    console.log('动画开始')
+  }
+})
 gsap.to(cube.rotation, {
   x: 2 * Math.PI,
   duration: 5
 })
 
 // 监听双击
-window.addEventListener('dblclick', () => {
-  const fullScreenElement = document.fullscreenElement
-  console.log('fullScreenElement', fullScreenElement)
-  // 控制屏幕进入全屏，退出全屏
-  if (fullScreenElement) {
-    document.exitFullscreen()
+window.addEventListener("dblclick", () => {
+  if (animate.isActive()) {
+    // 暂停
+    animate.pause();
   } else {
-    renderer.domElement.requestFullscreen()
+    // 恢复
+    animate.resume();
   }
 })
 
@@ -106,7 +94,7 @@ function render() {
   // 使用渲染器，通过相机将场景渲染进来
   renderer.render(scene, camera)
   // 渲染下一帧的时候就会调用render函数
-  requestAnimationFrame(render)
+  requestAnimationFrame( render )
 }
 render()
 
@@ -121,10 +109,7 @@ window.addEventListener('resize', () => {
   // 设置渲染器的像素比
   renderer.setPixelRatio(devicePixelRatio)
 })
+
 </script>
 
-<template>
-  <div></div>
-</template>
-
-<style scoped></style>
+<template><div></div></template>
