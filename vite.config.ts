f30abfcfa9e4dfa75527ab0372e9ed343491b51e
pmm0316 <847679250@qq.com>
2024-09-16 16:03:46 +0800

    build: 配置base

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  publicDir: '/vue-hobby',
+  base:  '/vue-hobby/',
   plugins: [
     vue(),
     cesium(),
